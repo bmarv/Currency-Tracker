7d7aaf5a4536242356f7f0aadf15cbae4393e089
bmarv <beesemarvin@gmail.com>
2020-06-23 15:46:13 +0200

    Project structure build

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>NavBar</h1>
+                <h1>CurrencyPicker</h1>
+                <h1>CurrCard</h1>
+                <h1>CurrCard</h1>
+                <h1>CurrGraph</h1>
+                <h1>Footer</h1>
+            </div>  
+        )
+    }
 }
 
-export default App;
+export default App
